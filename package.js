0b07091426a9d4a885cecc576068d2758e9001d1
PolGuixe <pol@zetoff.com>
2016-04-28 17:48:33 +0200

    initial package.json

--- package.js
+++ package.js
@@ -2,23 +2,31 @@ Package.describe({
   name: 'zetoff:meteor-astroforms-react-bootstrap',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Astroforms-React-Bootstrap implmentation',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Zetoff/meteor-astroforms-react-bootstrap',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
-  api.use('ecmascript');
-  api.mainModule('meteor-astroforms-react-bootstrap.js');
-});
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('zetoff:meteor-astroforms-react-bootstrap');
-  api.mainModule('meteor-astroforms-react-bootstrap-tests.js');
+//meteor dependencies
+  api.use(['ecmascript',
+'es5-shim',
+'tmeasday:check-npm-versions',
+'zetoff:astroforms-react'
+],['client', 'server']);
+//TODO: really need server? If eliminated, does imply that the dependencies will not we executed in the server?
+
+//npm dependencies
+// using tmeasday:check-npm-versions
+
+api.addFiles('client/main.js', 'client');
+
+ api.export('Astroforms','client');
+
+  api.mainModule('lib/main.js',['client','server']);
 });

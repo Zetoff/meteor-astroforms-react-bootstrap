fa34ff0e9392d91c50efd7f280b4193ae2d937e8
PolGuixe <pol@zetoff.com>
2016-04-28 19:28:40 +0200

    added basic html component
    need to be replaced with react-bootstrap components

---
+++ lib/inputs/components/textfield.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// testing purpose only. this should not be here on the final release
+class TextField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentWillReceiveProps(props) {
+    // makes component reactive to db changes
+    this.updateValue(props.value);
+  }
+  handleChange(event) {
+    // user updates
+    this.updateValue(event.target.value);
+  }
+  updateValue(value) {
+    this.setState({
+      value: value
+    });
+  }
+  render() {
+    return (
+      <div className="textfield-default">
+        <label>{this.props.label}&nbsp;</label>
+        {this.props.prefix}<input {...this.props.htmlAttributes} value={this.state.value} onChange={this.handleChange} /> {this.props.suffix}
+      </div>
+    );
+  }
+}
+
+export default TextField;

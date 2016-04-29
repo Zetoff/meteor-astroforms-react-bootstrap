29267dcb1b1a32ae3d073f6f3e4dba461f76a854
PolGuixe <pol@zetoff.com>
2016-04-29 16:29:49 +0200

    added lodash dependency

--- check-npm.js
+++ check-npm.js
@@ -2,5 +2,6 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 
 checkNpmVersions({
   "react": ">=0.14.7 || ^15.0.0-rc.2",
-  "react-bootstrap": ">=0.29.0"
+  "react-bootstrap": ">=0.29.0",
+  "lodash": ">=4.11.0"
 });

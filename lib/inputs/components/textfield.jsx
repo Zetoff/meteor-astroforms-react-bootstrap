e940861d294e9ab884f9b1aa7c6b34fc2b92cdcf
ochicf <oscarchicfarre@gmail.com>
2016-04-29 17:48:08 +0200

    use field update

--- lib/inputs/components/textfield.jsx
+++ lib/inputs/components/textfield.jsx
@@ -4,46 +4,45 @@ import {FormGroup, ControlLabel, FormControl, InputGroup} from 'react-bootstrap'
 class TextField extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: props.value
-    };
     this.handleChange = this.handleChange.bind(this);
   }
   componentWillReceiveProps(props) {
     // makes component reactive to db changes
-    this.updateValue(props.value);
+    // this.updateValue(props.value);
   }
   handleChange(event) {
-    // user updates
-    this.updateValue(event.target.value);
+    this.props.field.update(event.target.value);
   }
-  updateValue(value) {
-    this.setState({value: value});
+  renderFormControl() {
+    return(<FormControl {...this.props.htmlAttributes} onChange={this.handleChange} defaultValue={this.props.value} />);
   }
-
-  renderFormControl(){
-    return(<FormControl {...this.props.htmlAttributes} value={this.state.value} type="text" placeholder="Enter text"/>);
-  }
-
-  renderFormWithOrWithoutPrefixOrSuffix(){
+  renderInput(){
     if(this.props.suffix || this.props.prefix){
-return(
-      <InputGroup>
-      {this.props.prefix ? <InputGroup.Addon>{this.props.prefix}</InputGroup.Addon> : null}
-      {this.renderFormControl()}
-      {this.props.suffix ? <InputGroup.Addon>{this.props.suffix}</InputGroup.Addon> : null}
-      </InputGroup>
-    );
-  }else{
-    return this.renderFormControl();
+      return(
+        <InputGroup>
+        {this.props.prefix ? <InputGroup.Addon>{this.props.prefix}</InputGroup.Addon> : null}
+        {this.renderFormControl()}
+        {this.props.suffix ? <InputGroup.Addon>{this.props.suffix}</InputGroup.Addon> : null}
+        </InputGroup>
+      );
+    } else{
+      return this.renderFormControl();
+    }
   }
+  renderValidationMessage() {
+    const { validation } = this.props;
+    if (validation.state && validation.message) {
+      return (
+        <span className="validation-message">{validation.message}</span>
+      );
+    }
   }
-
   render() {
     return (
-      <FormGroup className="textfield-default" controlId="formControlsText">
+      <FormGroup className="textfield-default" controlId="formControlsText" validationState={this.props.validation.state}>
         <ControlLabel>{this.props.label}</ControlLabel>
-        {this.renderFormWithOrWithoutPrefixOrSuffix()}
+        {this.renderValidationMessage()}
+        {this.renderInput()}
       </FormGroup>
     );
   }
